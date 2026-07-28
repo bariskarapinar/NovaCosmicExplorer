59cc92b57f31cdffd8a308c815251642e7623cf8
bariskarapinar <bkarapinar1@hotmail.com>
2026-07-28 13:32:50 +0300

    Color Update

---
+++ app/src/main/java/com/cosmic/nova/ui/theme/Color.kt
@@ -0,0 +1,20 @@
+package com.cosmic.nova.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Purple80 = Color(0xFFD0BCFF)
+val PurpleGrey80 = Color(0xFFCCC2DC)
+val Pink80 = Color(0xFFEFB8C8)
+
+val Purple40 = Color(0xFF6650a4)
+val PurpleGrey40 = Color(0xFF625b71)
+val Pink40 = Color(0xFF7D5260)
+
+// Cosmic Explorer Palette
+val DeepSpace = Color(0xFF0B0D17)
+val NebulaPurple = Color(0xFF4F359B)
+val StarlightBlue = Color(0xFF90CAF9)
+val SolarGold = Color(0xFFFFD54F)
+val EarthEmerald = Color(0xFF4DB6AC)
+val MarsRed = Color(0xFFFF5252)
+val SurfaceGray = Color(0xFF1E1E2C)
